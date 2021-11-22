b0ace91a7db83864d0cb416a075d9f21557394b1
arashi87 <arasi27676271@gmail.com>
2021-11-23 00:41:03 +0800

    feat: first commit

---
+++ src/module/gui.py
@@ -0,0 +1,45 @@
+from functools import partial
+
+from PyQt5.QtWidgets import (QGroupBox, QHBoxLayout, QPushButton, QVBoxLayout,
+                             QWidget)
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # layout setting
+        self.padding = 20
+        self.row_amount = 4
+        self.col_amount = 4
+        self._height = 400
+        self._width = 500
+
+        # window setting
+        self.layout = QHBoxLayout()
+        self.setLayout(self.layout)
+        self.setWindowTitle("HW1-5")
+        self.setGeometry(0, 0, self._width, self._height)
+        self.init_btn()
+        self.show()
+
+    def init_btn(self):
+        msgs = [
+            "1. Show Train Images",
+            "2. Show HyperParameter",
+            "3. Show Model Struct",
+            "4. Show Accuracy",
+        ]
+        v_layout = QVBoxLayout()
+        group_box = QGroupBox("VGG16 TEST")
+        for i in range(len(msgs)):
+            btn = QPushButton(msgs[i], self)
+            btn.clicked.connect(partial(self._executor, i))
+            v_layout.addWidget(btn)
+        group_box.setLayout(v_layout)
+        self.layout.addWidget(group_box)
+
+    def _executor(self, i):
+        func = []
+        if i < len(func):
+            func[i]()
